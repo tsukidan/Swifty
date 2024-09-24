2ef603d1cc19d330f6121704d65fcf1d5cee4d28
Daniel Navarro <60456378+Sakezone@users.noreply.github.com>
2024-09-23 18:09:22 -0700

    fix prev files for dark mode update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "./(marketing)/_components/providers/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,11 +39,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider 
+        attribute="class"
+        defaultTheme="system" 
+        enableSystem 
+        disableTransitionOnChange
+        storageKey="swifty-theme">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
